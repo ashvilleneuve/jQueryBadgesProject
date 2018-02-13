245428fc93035bf1abf0bf2842d8dc62c913e1c3
ashvilleneuve <ashleyvilleneuve@icloud.com>
2018-02-13 00:30:50 -0800

    Fixing things to pass course check, hopefully

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+     url: 'https://www.codeschool.com/users/ashvill.json',
+     dataType: 'jsonp',
+     success: function(response) {
+       $(response.courses.completed).each(function(i, obj){
+         var obj = response.courses.completed;
+        $('#badges').append('<div class="course"><h3>' + obj[i].title + '</h3><img src="' + obj[i].badge + '"><a href="' + obj[i].url + '" class="btn btn-primary" target="_blank">See Course</a></div>')
+      });
+     }
+   });
 });
